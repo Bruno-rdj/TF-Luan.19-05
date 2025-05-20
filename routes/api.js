ab0447ca86d001cf3bb6e1ded607e7b4f39a7338
Bruno Rocha <brunorochaj250705@gmail.com>
2025-05-20 19:09:25 -0300

    finalização do tf do luan do dia 19/05

--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,19 @@
 import { Router } from 'express';
 import ApiController from '../app/Controllers/ApiController.js';
 import TodoApiController from '../app/Controllers/TodoApiController.js';
+import ControladoresModel from '../app/Models/ColaboradoresModel.js';
+import ControladoresApiController from '../app/Controllers/ColaboradoresApiController.js';
+import ColaboradoresApiController from '../app/Controllers/ColaboradoresApiController.js';
 
 export default (function () {
 
     const router = Router();
 
-    router.get('/todos', TodoApiController.list);
+    router.get('/colaboradores', ColaboradoresApiController.list);
 
-    router.get('/todos/:id', TodoApiController.get);
+    router.get('/colaboradores/:id', ColaboradoresApiController.get);
 
-    router.post('/todos', TodoApiController.insert);
-
-    router.put('/todos/:id', TodoApiController.update);
-
-    router.delete('/todos/:id', TodoApiController.delete);
+    router.post('/colaboradores', ColaboradoresApiController.insert);
 
     return router;
 
